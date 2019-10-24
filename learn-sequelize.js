26e498fcb7e554bb671c3bf4aa7f49967882e78f
Simon <simonhe1@mail.com>
2019-10-24 07:52:54 -0400

    Finished all test cases for sequelize practice.

--- learn-sequelize.js
+++ learn-sequelize.js
@@ -1,11 +1,11 @@
-const { Genre } = require('./models');
+const { Genre, Actor, Movie } = require('./models');
 
 /*
-  currently, the genre table only has: Action, Adventure, and Drama
+  currently, the genre table only has: Action, Comedy, and Drama
   Add one more Genre.
 */
 function insertNewGenre() {
-
+  return Genre.create({ name: 'Horror'});
 }
 
 /*
@@ -13,51 +13,79 @@ function insertNewGenre() {
   Add one more Movie of your choice. But it CANNOT be from 2008.
 */
 function insertNewMovie() {
-
+  return Movie.create({title: 'The Dark Knight Rises', year: 2012});
 }
 
 /*
   Return the title of the movie with ID=2
 */
 function getMovieWithId2() {
-
+  return Movie.findOne({
+    where:{
+      id: [2]
+    }
+  }).then(movie => {
+    return movie.title
+  });
 }
 
 /*
   Return an array of all the actor names
 */
 function getAllActors() {
-
+  return Actor.findAll().then(actor =>{
+    actor = actor.map(act =>{
+      return act.name;
+    });
+    return actor;
+  });
 }
 
 /*
   Return an array of all the movie names from 2008
 */
 function getAllMoviesFrom2008() {
-
+  return Movie.findAll({
+    where : {
+      year : [2008],
+    }
+  }).then(movie => {
+    movie = movie.map(mov => {
+      return mov.title;
+    })
+    return movie;
+  });
 }
 
 /*
   Delete the genre you added in the first test
 */
 function deleteGenreYouAdded() {
-
+  return Genre.destroy({
+    where: {
+      name: ['Horror'],
+    }
+  });
 }
 
 /*
   Rosario Dawson acted in the movie Eagle Eye.
   Add this association.
 */
-function associateRosarioToEagleEye() {
-
+async function associateRosarioToEagleEye() {
+  let actor = await Actor.findOne({where: {name: 'Rosario Dawson'}});
+  let movie = await Movie.findOne({where: {title: 'Eagle Eye'}});
+  return actor.addMovie(movie);
 }
 
 /*
   Robert Downey Jr. acted in the movie Tropic Thunder.
   Add this association.
 */
-function associateRobertToTropicThunder() {
-
+async function associateRobertToTropicThunder() {
+  let actor = await Actor.findOne({where: {name: 'Robert Downey Jr.'}});
+  let movie = await Movie.findOne({where: {title: 'Tropic Thunder'}});
+  return actor.addMovie(movie);
 }
 
 module.exports = {
